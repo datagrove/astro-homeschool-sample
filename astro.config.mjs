831d7d4c5e0b86e7c50d512010f4b9cad0d5206b
Meagan <meagan.h.smith@gmail.com>
2023-05-25 10:09:19 -0400

    FAQ page - shift to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs(), mdx()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
